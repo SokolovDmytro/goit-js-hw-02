6ffa69e4dd452a5f274eef7be9e0c83037430797
sokolovdmytro <sokolov.dmytro.an@gmail.com>
2020-04-19 22:26:11 +0300

    added task 5-6

---
+++ js/task-06.js
@@ -0,0 +1,30 @@
+// 'use strict';
+
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// do {
+//     input = prompt('Введите число');
+
+//     if (input === null) {
+//         alert('Пользователь нажал Cancel')
+//         break;
+//     }
+//     //  console.log(input);
+//     input = Number(input);
+//     const notNamber = Number.isNaN(input);
+
+//     if (notNamber) {
+//         alert('Было введено не число, попробуйте еще раз');
+//         continue;
+//     }
+//     // console.log(input);
+//     numbers.push(input);
+// } while (input !== null);
+
+// for (const number of numbers) {
+//     total += number;
+// }
+
+// console.log('Общая сумма чисел равна' + ' ' + total);
